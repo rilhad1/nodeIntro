46f224d5cb30de85b6b2627947e5ee6acd8a486d
Volodumur Boychuk <rilhad1@gmail.com>
2018-04-25 15:02:31 +0000

    end

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const urlWeather = (lat, lng) => {
     return "https://api.darksky.net/forecast/31310dfb5c7a7cedcf99c445a6136c0b/" + lat + ',' + lng;
 };
 
-console.time('qqq')
 fetch(urlCoordinates(city))
     .then(response => response.json())
     .then(json => json.results[0].geometry.location)
@@ -21,7 +20,7 @@ fetch(urlCoordinates(city))
             let weather = 'In general, the weather in ' + city + ' is ' + json.currently.summary + '.' +
                 ' The current temperature is ' + json.currently.temperature + '.' +
                 ' The humidity level is ' + json.currently.humidity + '.' +
-                ' The wind speed is ' + json.currently.windSpeed + '.' + 
+                ' The wind speed is ' + json.currently.windSpeed + '.' +
                 ' Pressure is' + json.currently.pressure;
             fs.writeFileSync('weather.json', JSON.stringify(json,
                 function(key, value) {
@@ -33,10 +32,8 @@ fetch(urlCoordinates(city))
                 }, 2));
             fs.writeFile("weather.txt", weather, function(err) {
                 if (err) {
-                    return console.log(err);
+                    return console.error(err);
                 }
             })
-            console.timeEnd('qqq')
-            ;
         })
     );
